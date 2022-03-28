4de3729be6788c50a38948acc289555fbb48c1f7
AK7795 <akshaysingodi@gmail.com>
2022-03-28 14:40:39 +0530

    done2

--- calci.py
+++ calci.py
@@ -13,3 +13,20 @@ def mul(x, y):
 
 def div(x, y):
     return x/y
+
+
+
+
+if __name__ == "__main__":
+    n1 = int(input("enter n1 : "))
+    n2 = int(input("enter n2 : "))
+
+    r1 = add(n1,n2)
+    r2 = sub(n1, n2)
+    r3 = mul(n1, n2)
+    r4 = div(n1, n2)
+
+    print(r1)
+    print(r2)
+    print(r3)
+    print(r4)
